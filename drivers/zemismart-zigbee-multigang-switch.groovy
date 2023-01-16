4c0ec92fc4ee40ee587a59552c3df31d08c742c6
kkossev <kossevkrassimir@gmail.com>
2023-01-16 13:12:50 +0200

    Ver. 0.3.0  2023-01-07 kkossev - noBindingButPolling() for _TZ3000_fvh3pjaz _TZ3000_9hpxg80k _TZ3000_wyhuocal

--- drivers/zemismart-zigbee-multigang-switch.groovy
+++ drivers/zemismart-zigbee-multigang-switch.groovy
@@ -23,6 +23,7 @@
  *  Ver. 0.2.13 2022-11-12 kkossev - tuyaBlackMagic() for Xenon similar to Tuya Metering Plug; _TZ3000_cfnprab5 fingerprint correction; added SiHAS and NodOn switches
  *  Ver. 0.2.14 2022-11-23 kkossev - added 'ledMOode' command; fingerprints critical bug fix.
  *  Ver. 0.2.15 2022-11-23 kkossev - added added _TZ3000_zmy1waw6
+ *  Ver. 0.3.0  2023-01-07 kkossev - noBindingButPolling() for _TZ3000_fvh3pjaz _TZ3000_9hpxg80k _TZ3000_wyhuocal
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -38,8 +39,8 @@ import hubitat.device.HubAction
 import hubitat.device.Protocol
 import groovy.transform.Field
 
-def version() { "0.2.14" }
-def timeStamp() {"2022/11/23 6:47 PM"}
+def version() { "0.3.0" }
+def timeStamp() {"2023/01/07 9:43 PM"}
 
 @Field static final Boolean debug = false
 
@@ -108,6 +109,7 @@ metadata {
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,E000,E001,0000", outClusters:"0019,000A", model:"TS0011", manufacturer:"_TZ3000_hhiodade", deviceJoinName: "Moes ZTS-EU_1gang"  
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,0000",           outClusters:"0019,000A", model:"TS0011", manufacturer:"_TZ3000_hhiodade", deviceJoinName: "Moes ZTS-EU_1gang"  // https://community.hubitat.com/t/uk-moes-zigbee-1-2-3-or-4-gang-light-switch/89747/5?u=kkossev
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,E000,E001,0000", outClusters:"0019,000A", model:"TS0011", manufacturer:"_TZ3000_ji4araar", deviceJoinName: "Tuya 1 gang switch"  
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,0000",           outClusters:"0019,000A", model:"TS0011", manufacturer:"_TZ3000_9hpxg80k", deviceJoinName: "Tuya 1 gang"      // https://github.com/zigpy/zha-device-handlers/issues/535
         
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0001,0007,0000,0003,0004,0005,0006,E000,E001,0002", outClusters:"0019,000A", model:"TS0012", manufacturer:"_TZ3000_k008kbls", deviceJoinName: "Zemismart Zigbee Switch Multi-Gang" // check! 
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0004,0005,0006",           outClusters:"0019",      model:"TS0012", manufacturer:"_TZ3000_uz5xzdgy", deviceJoinName: "Zemismart Zigbee Switch No Neutral"
@@ -132,7 +134,10 @@ metadata {
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0004,0005,0006",           outClusters:"0019",      model:"TS0013", manufacturer:"_TZ3000_k44bsygw", deviceJoinName: "Zemismart Zigbee Switch No Neutral"
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,0000",           outClusters:"0019,000A", model:"TS0013", manufacturer:"_TZ3000_qewo8dlz", deviceJoinName: "Tuya Zigbee Switch 3 Gang No Neutral"    // @dingyang.yee https://www.aliexpress.com/item/4000298926256.html https://github.com/Koenkk/zigbee2mqtt/issues/6138#issuecomment-774720939
         
+        /* these do NOT work with Hubitat !
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0003,0004,0005,0006,E000,E001,0000", outClusters:"0019,000A", model:"TS011F", manufacturer:"_TZ3000_cfnprab5", deviceJoinName: "Xenon 4-gang + 2 USB extension"    //https://community.hubitat.com/t/xenon-4-gang-2-usb-extension-unable-to-switch-off-individual-sockets/101384/14?u=kkossev
+        fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0006,0003,0004,0005,E001",      outClusters:"0019,000A", model:"TS011F", manufacturer:"_TZ3000_cfnprab5", deviceJoinName: "Xenon 4-gang + 2 USB extension"    //https://community.hubitat.com/t/xenon-4-gang-2-usb-extension-unable-to-switch-off-individual-sockets/101384/14?u=kkossev
+        */
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0004,0005,0006",                outClusters:"0019,000A", model:"TS011F", manufacturer:"_TZ3000_zmy1waw6", deviceJoinName: "Moes 1 gang"                       // https://github.com/zigpy/zha-device-handlers/issues/1262
         fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,000A,0004,0005,0006",           outClusters:"0019",      model:"TS0115", manufacturer:"_TYZB01_vkwryfdr", deviceJoinName: "UseeLink Power Strip"              //https://community.hubitat.com/t/another-brick-in-the-wall-tuya-joins-the-zigbee-alliance/44152/28?u=kkossev
         
@@ -164,9 +169,10 @@ metadata {
         input (name: "txtEnable", type: "bool", title: "Enable description text logging", defaultValue: true)
         input (title: "IMPORTANT", description: "<b>In order to operate normally, please pair the device to HE after changing to this driver!</b>", type: "paragraph", element: "paragraph")        
     }
- 
 }
 
+private boolean isHEProblematic() {device.getDataValue("manufacturer") in ["_TZ3000_okaz9tjs", "_TZ3000_r6buo8ba", "_TZ3000_cfnprab5", "SONOFF", "Woolley", "unknown"] }
+private boolean noBindingButPolling() {device.getDataValue("manufacturer") in ['_TZ3000_fvh3pjaz', '_TZ3000_9hpxg80k', '_TZ3000_wyhuocal']}    //0x4001 OnTime: value 0 //0x4002 OffWaitTime: value 0
 
 // Parse incoming device messages to generate events
 
@@ -181,7 +187,7 @@ def parse(String description) {
        if (settings?.logEnable) log.warn "${device.displayName} exception caught while parsing description ${description} \r descMap:  ${descMap}"
        return null
    }    
-   logDebug "Parsed: $descMap"
+   logDebug "Parsed descMap: ${descMap}"
     
    Map map = null // [:]
         
@@ -230,6 +236,12 @@ def parse(String description) {
     }
     else if (descMap.cluster == "E001") { // Tuya Switch Mode cluster
         processOnOfClusterOtherAttr( descMap )
+    }
+    else if (descMap?.clusterId == "0013" &&  descMap?.profileId != null && descMap?.profileId == "0000" ) {
+    /*
+        log.warn "leave!"
+        configure()
+        */
     }
     else {
         logDebug "${device.displayName} unprocessed EP: ${descMap.sourceEndpoint} cluster: ${descMap.clusterId} attrId: ${descMap.attrId}"
@@ -392,12 +404,9 @@ def updated() {
     logDebug "Parent updated"
 }
 
-
 def tuyaBlackMagic() {
     List<String> cmds = []
     cmds += zigbee.readAttribute(0x0000, [0x0004, 0x000, 0x0001, 0x0005, 0x0007, 0xfffe], [:], delay=200)
-    cmds += zigbee.writeAttribute(0x0000, 0xffde, 0x20, 0x0d, [destEndpoint :0x01], delay=50) 
-    cmds += zigbee.writeAttribute(0x0000, 0xffde, 0x20, 0x0d, [destEndpoint :0x01], delay=50) 
     cmds += zigbee.writeAttribute(0x0000, 0xffde, 0x20, 0x0d, [destEndpoint :0x01], delay=50) 
     return cmds
 }
@@ -405,10 +414,23 @@ def tuyaBlackMagic() {
 def configure() {
     logDebug " configure().."
     List<String> cmds = []
+    if (device.data.manufacturer in ["_TZ3000_cfnprab5", "_TZ3000_okaz9tjs"]) {
+        log.warn "this device ${device.data.manufacturer} is known to NOT work with HE!"
+    }
     cmds += tuyaBlackMagic()
-    //cmds += refresh()
-    cmds += zigbee.onOffConfig()
-    cmds += zigbee.onOffRefresh()
+    if (noBindingButPolling()) {
+		//these  will send out device anounce message at ervery 2 mins as heart beat, setting 0x0099 to 1 will disable it.
+		cmds += zigbee.writeAttribute(zigbee.BASIC_CLUSTER, 0x0099, 0x20, 0x01, [mfgCode: 0x0000])
+        // Hack : Need to disable reporting for thoses devices, else It will enable a auto power off after 2mn.     // see https://github.com/dresden-elektronik/deconz-rest-plugin/issues/3693
+        // https://github.com/Mariano-Github/Edge-Drivers-Beta/blob/652bcfbcf7b8ab8a14557e097b740216760f2b02/zigbee-multi-switch-v4-childs/src/init.lua 
+        log.warn "disabling ${device.data.manufacturer} device announce message every 2 mins and skipping reporting configuiration!"
+        cmds += zigbee.onOffRefresh()
+    }
+    else {
+        //cmds += refresh()
+        cmds += zigbee.onOffConfig()
+        cmds += zigbee.onOffRefresh()
+    }        
     sendZigbeeCommands(cmds)
 }
 
